6dc10a0391ac0f0e225d129bc3320e872abfdb58
AwaisQazii <awaisqazi6391@gmail.com>
2022-11-18 11:29:05 +0500

    new

---
+++ lib/controllers/home_controller.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:get/get.dart';
+
+class HomeController extends GetxController {
+  TextEditingController name = TextEditingController();
+  TextEditingController username = TextEditingController();
+  TextEditingController email = TextEditingController();
+
+
+
+
+
+}
---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'dart:math';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_demo/screens/home_screen.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'demo app',
+      onGenerateRoute: (settings) =>
+          MaterialPageRoute(builder: (context) => HomeScreen()),
+    );
+  }
+}
