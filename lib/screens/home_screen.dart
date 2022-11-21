8b6b252aefc70668e7419d7398fc8f1a2fe3098e
AwaisQazii <awaisqazi6391@gmail.com>
2022-11-21 11:49:48 +0500

    testing 123

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -12,7 +12,7 @@ class HomeScreen extends StatelessWidget {
       appBar: AppBar(
         centerTitle: true,
         title: const Text(
-          'Movies Collection',
+          'BLUE COLLECTION',
         ),
       ),
     );
